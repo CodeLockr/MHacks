193c68d355b4ab0185289ae639a2ed42bd5decfc
CodeLockr <matthewlocker1@Matthews-MacBook-Air.local>
2016-10-08 01:08:25 -0400

    Basic App

---
+++ MHacks/app/src/main/java/com/example/matthewlocker1/mhacks/PromptPage.java
@@ -0,0 +1,13 @@
+package com.example.matthewlocker1.mhacks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PromptPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prompt_page);
+    }
+}
